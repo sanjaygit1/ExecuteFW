2df58c917ffcdbc87f2ce4cfd3722803c2de33ba
sanjaygit1 <rathinreddy1402@gmail.com>
2025-04-21 19:27:51 +0530

    push1

---
+++ AdavnceSelenium_QCO-SOEAJD-M22/src/test/java/Assignments/DebuggingsEx.java
@@ -0,0 +1,22 @@
+package Assignments;
+
+public class DebuggingsEx {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+		int temp;
+		temp=a;
+		a=b;
+		b=temp;
+		
+		System.out.println(a);
+		System.out.println(b);	
+	}
+
+}
